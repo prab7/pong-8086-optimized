8291609a26632a0e579d98df7be33c46885d885e
prab7 <nicksingh1109@gmail.com>
2025-04-07 16:46:16 +0530

    perf: Direct VGA memory writes (0A000h); feat: add wall collision logic and delay_loop instead of system time

--- pong.asm
+++ pong.asm
@@ -1,108 +1,166 @@
+this is the next iteration of code 
+
 .model small
-.stack 64
+.stack 64h
+.186
 
 .data
-time_aux db 0 ; check time change
-BALLX dw 0Ah  ; x_pos
-BALLY dw 0Ah  ; y_pos
-PREV_BALLX dw 0Ah  ; previous x_pos
-PREV_BALLY dw 0Ah  ; previous y_pos
-BALL_SIZE dw 05h
-speedX dw 05h
-speedY dw 02h
+    ball_x dw 0Ah      ; y_pos init
+    ball_y dw 0Ah      ; x_pos init
+    ball_size dw 10   
+    ball_dx dw 1            ; dx pixels
+    ball_dy dw 1            ; dy pixels
+    prev_ballx dw 1 dup(?)
+    prev_bally dw 1 dup(?)
+    color db 0bh      ; color
+    
 
 .code
 .startup
-    main:
-        call clear_screen
 
-        check_time:
-            mov ah,01h ;wait for a key press
-            int 16h
-            jnz exit
+    mov ax, 13h ;set video mode: 320x200 256 colors 
+    int 10h
 
-            mov ah,2ch ;get system time
-            int 21h
+    call clear_screen
 
-            cmp dl,time_aux
-            JE check_time
-            mov time_aux,dl ;update time
+main_loop:
 
-        call clear_ball  ; Clear the ball at its previous position
+    call clear_ball     ;clear previous ball
+    call draw_ball      ;draw new ball
 
-        ; Update the ball's position
-        mov ax,speedX
-        add BALLX,ax
-        mov ax,speedY
-        add BALLY,ax
+    ; update the previous position
+    mov ax, ball_x
+    mov prev_ballx,ax
+    mov ax, ball_y
+    mov prev_bally,ax
 
-        call draw_ball  ; Draw the ball at its new position
+    ; update ball position
+    mov ax, ball_dx
+    add ball_x, ax
+    mov ax, ball_dy
+    add ball_y, ax
 
-        ; Update the previous position
-        mov ax, BALLX
-        mov PREV_BALLX, ax
-        mov ax, BALLY
-        mov PREV_BALLY, ax
+    call check_collisions
 
-        jmp check_time
-
-    exit:
-        mov ax,3 ;return to text mode 
-        int 10h
-.exit
-        
-    draw_ball:
-        mov cx, BALLX ;x_pos init
-        mov dx, BALLY ;y_pos init
-        
-        horizontal:
-            mov ax,0C0Fh ;write graphics pixel, white
-            mov bh,00h ;page number
-            int 10h
-            inc cx
-            mov ax,cx       ; cx - ballx >= ball_size ? next line : next column
-            sub ax, BALLX
-            cmp ax, BALL_SIZE
-            jnge horizontal
-
-        vertical:
-            mov cx, BALLX ; cx back to init
-            inc dx        ; advance one line
-            mov ax,dx     ; dx - ballx >= ball_size ? exit : next line
-            sub ax, BALLY
-            cmp ax, BALL_SIZE
-            jnge horizontal
-        ret
+    call delay      ;Artificial delay
     
-    clear_ball:
-        mov cx, PREV_BALLX ;x_pos init
-        mov dx, PREV_BALLY ;y_pos init
-        
-        clear_horizontal:
-            mov ax,0C00h ;write graphics pixel, black
-            mov bh,00h ;page number
-            int 10h
-            inc cx
-            mov ax,cx       ; cx - prev_ballx >= ball_size ? next line : next column
-            sub ax, PREV_BALLX
-            cmp ax, BALL_SIZE
-            jnge clear_horizontal
+    mov ah, 01h       ;wait for a key press
+    int 16h
+    jz main_loop      ;continue if no key pressed
 
-        clear_vertical:
-            mov cx, PREV_BALLX ; cx back to init
-            inc dx        ; advance one line
-            mov ax,dx     ; dx - prev_bally >= ball_size ? exit : next line
-            sub ax, PREV_BALLY
-            cmp ax, BALL_SIZE
-            jnge clear_horizontal
+    ; Exit the program
+    mov ax, 03h       ; Set text mode (mode 03h)
+    int 10h
+
+    .exit
+
+
+clear_screen:
+    mov ax, 0A000h    ; Video memory segment
+    mov es, ax
+    xor di, di        
+    mov cx, 320*200   
+    xor al, al        ;color: black
+    rep stosb         ;fill video memory with black
+    ret
+
+
+draw_ball:
+    mov ax, 0A000h    
+    mov es, ax
+    mov bx, ball_y    
+    mov ax, 320
+    mul bx
+    add ax, ball_x    
+    mov di, ax        
+
+    mov cx, ball_size 
+
+    draw_ball_row:
+        push cx
+        mov cx, ball_size  
+        mov al, color      
+        rep stosb          
+        add di, 320        
+        sub di, ball_size  
+        pop cx
+        loop draw_ball_row
         ret
 
-    clear_screen:
-        mov ax,13h ;Set video mode ;320x200, 256 colors
-        int 10h
+clear_ball:
+    mov ax, 0A000h    
+    mov es, ax
+    mov bx, prev_bally
+    mov ax, 320
+    mul bx
+    add ax, prev_ballx
+    mov di, ax
+    
+    mov cx, ball_size
 
-        mov ah,0Bh ;set background color
-        xor bx,bx  ;black
-        int 10h
+    clear_row:
+        push cx
+        mov cx, ball_size
+
+        clear_pixel:
+            mov dx, prev_ballx
+            cmp dx, ball_x
+            jne write_black     ;if prev_ballx != ball_x
+            mov dx, prev_bally  
+            cmp dx, ball_y
+            jne write_black     ;if prev_bally != ball_y
+
+            inc di
+            loop clear_pixel    ; else skip
+            
+        next_jump:
+        add di, 320         ; next row
+        sub di, ball_size   ; set pointer at start location
+        pop cx
+        loop clear_row
         ret
+
+        write_black:
+            xor al,al           ;if !(prev_ballx == ball_x AND prev_bally == ball_y)
+            mov es:[di],al
+            inc di
+            loop clear_pixel
+            jmp next_jump       ;jump back to exit/loop properly
+
+
+check_collisions: 
+    cmp ball_x, 0
+    jle reverse_x     ; If ball_x <= 0, reverse x
+    mov ax, 320
+    sub ax, ball_size
+    cmp ball_x, ax
+    jge reverse_x     ; If ball_x >= 320 - ball_size, reverse x
+
+    cmp ball_y, 0
+    jle reverse_y     ; If ball_y <= 0, reverse y
+    mov ax, 200
+    sub ax, ball_size
+    cmp ball_y, ax
+    jge reverse_y     ; If ball_y >= 200 - ball_size, reverse y
+    ret
+
+    reverse_x:
+        neg ball_dx       ;reverse x
+        ret
+
+    reverse_y:
+        neg ball_dy       ;reverse y
+        ret
+
+delay:
+    mov cx, 01h       ;delay counter (adjust for speed)
+    delay_loop:
+        push cx
+        mov cx, 0FFFFh
+    inner_delay:
+        loop inner_delay
+        pop cx
+        loop delay_loop
+        ret
+
 end
